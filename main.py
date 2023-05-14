afd7c20fdc31e8c2ed83946e1ce34b429953db5e
Daniel Gross <danielgross@dhcp-10-105-41-149.wireless.northwestern.private>
2023-05-14 16:44:52 -0500

    populating main

--- main.py
+++ main.py
@@ -9,16 +9,25 @@ current_folder = os.getcwd()
 CDs_folder = 'CDs_and_Vinyl'
 
 # Open and load json training files
+print("Importing Data...")
 x = pd.read_json(os.path.join(current_folder, CDs_folder, 'train', 'review_training.json'))
 y = pd.read_json(os.path.join(current_folder, CDs_folder, 'train', 'product_training.json'))
 
 # Other imports  
+print("Importing Required Modules...")
+import time
 import numpy as np
 from nltk import sentiment
 from sklearn.model_selection import cross_validate
 from sklearn import metrics
 import matplotlib.pyplot as plt
+plt.style.use('Solarize_Light2')
 from sklearn.model_selection import train_test_split
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import re
 
 # Patch to speed up sklearn (ONLY IF YOU HAVE AN INTEL CHIP)   
 from sklearnex import patch_sklearn 
@@ -27,18 +36,27 @@ patch_sklearn()
 # ## Pre Processing
 
 # Review Count
+print("Counting Reviews...")
+tic = time.perf_counter()
 dropUnverified = x[x.verified == True]
 reviewCount = x.groupby('asin')["reviewerID"].count()
 reviewCount = reviewCount.rename("Review_Count")
-
+toc = time.perf_counter()
+print(f"Reviews Counted in {toc - tic:0.4f} Seconds!" + "\n")
 
 # Percent verified
+print("Determining Percent Verified Reviews...")
+tic = time.perf_counter()
 percent_verified = dropUnverified.groupby("asin")["reviewerID"].count()
 percent_verified = percent_verified/reviewCount
 percent_verified = percent_verified.apply(lambda x: x if x > 0  else 0)
 percent_verified = percent_verified.rename("%_Verified")
+toc = time.perf_counter()
+print(f"Percent Verified Reviews Determined in {toc - tic:0.4f} Seconds!" + "\n")
 
 # Total number of votes across reviews
+print("Counting Total Votes Across Reviews...")
+tic = time.perf_counter()
 x_copy = x.copy(deep=True)
 total_review_num = x_copy.vote
 total_review_num = total_review_num.apply(lambda x: float(x.replace(",", "")) if type(x) == str  else 0)
@@ -46,25 +64,137 @@ total_review_num = total_review_num.rename("vote").to_frame()
 x_copy["vote"] = total_review_num["vote"]
 total_votes = x_copy.groupby('asin')["vote"].sum("vote")
 total_votes = total_votes.rename("Total_Votes")
+toc = time.perf_counter()
+print(f"Votes Counted in {toc - tic:0.4f} Seconds!" + "\n")
 
 # Text Analysis
 
 # Length of Reviews Feature
+print("Counting Length of Reviews...")
+tic = time.perf_counter()
 reviewlength = x.groupby('asin')["reviewText"].apply(lambda x: x.str.split().str.len().mean())
 reviewlength = reviewlength.fillna(0).rename("Review_Length")
+toc = time.perf_counter()
+print(f"Review Length Counted in {toc - tic:0.4f} Seconds!" + "\n")
 
+print("Counting Length of Summaries...")
+tic = time.perf_counter()
 summarylength = x.groupby('asin')["summary"].apply(lambda x: x.str.split().str.len().mean())
 summarylength = summarylength.fillna(0).rename("Summary_Length")
+toc = time.perf_counter()
+print(f"Summary Length Counted in {toc - tic:0.4f} Seconds!" + "\n")
 
 # Percentage Uppercase Feature:
+print("Calculating Percent Capital for Reviews...")
+tic = time.perf_counter()
 RpercentCap = x.groupby('asin')["reviewText"].apply(lambda x: (x.str.count("[A-Z]")/x.str.len()).mean())
 RpercentCap = RpercentCap.fillna(0).rename("Review_Percent_Uppercase")
+toc = time.perf_counter()
+print(f"Percent Capital for Reviews Calculated in {toc - tic:0.4f} Seconds!" + "\n")
 
+print("Calculating Percent Capital for Summaries...")
+tic = time.perf_counter()
 SpercentCap = x.groupby('asin')["summary"].apply(lambda x: (x.str.count("[A-Z]")/x.str.len()).mean())
 SpercentCap = SpercentCap.fillna(0).rename("Summary_Percent_Uppercase")
+toc = time.perf_counter()
+print(f"Percent Capital for Summaries Calculated in {toc - tic:0.4f} Seconds!" + "\n")
+
 
 
 # Sentiment Analysis
+
+## Helper function for sentiment analysis testing
+def review_sentiment(text):
+    sia = sentiment.SentimentIntensityAnalyzer()
+    return sia.polarity_scores(text)["compound"]
+
+import string
+def preprocess(text):
+    if (text == None): return ""
+    lemmatizer = WordNetLemmatizer()
+    text = "".join([char for char in text.lower() if char not in string.punctuation])
+    words = [word for word in word_tokenize(text) if word not in stopwords.words("english")]
+    tokens = [lemmatizer.lemmatize(token) for token in words]
+    return ' '.join(tokens)
+
+print("Processing Text... \n")
+print("This usually takes a while (~100 minutes)")
+print("Currently removing stop words and lemmatizing from the review and review summaries.")
+tic = time.perf_counter()
+
+x["processedText"] = x["reviewText"].apply(preprocess)
+x["processedSums"] = x["summary"].apply(preprocess)
+
+toc = time.perf_counter()
+print(f"Text Processed in {toc - tic:0.4f} Seconds!" + "\n")
+
+def replaceWords(text, pos_words, neg_words):
+    pos_message = "love best awesome"
+    neg_message = "terrible disgusting awful"
+
+    #replace
+    for pw in pos_words:
+        text = text.replace(" "+pw," "+pos_message)
+    for nw in neg_words:
+        text = text.replace(" "+nw," "+neg_message)
+
+#spaces added to prevent replacing parts of words. 
+# Still could replace ends of words, but probs not that much of an issue
+
+    #append
+    # for pw in pos_words:
+    #     if (" "+pw) in text: text += " "+pos_message 
+    # for nw in neg_words:
+    #     if (" "+nw) in text: text += " "+neg_message 
+
+    return text
+
+def get_disjoint_elements(arr1,arr2):
+    return [[e for e in arr1 if e not in arr2],[e for e in arr2 if e not in arr1]]
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+#use count vectorizer on all reviews, summaries
+
+def count_and_getMostCommon(textdf):
+    cv = CountVectorizer(ngram_range=(1,2), max_features=50) #max_features itself gets most common, but not sorted
+    #sum the resulting sparse matrices to get total counts
+    counts = cv.fit_transform(textdf).toarray().sum(axis=0)
+    #argsort to order by MOST COMMON 
+    return cv.get_feature_names_out()[np.argsort(counts)[::-1]]
+
+#split data into pos, neg reviews
+data_w_awesomeness = x.merge(y,"inner","asin")
+pos_revs = data_w_awesomeness.query("awesomeness==1")
+neg_revs = data_w_awesomeness.query("awesomeness==0")
+
+#get the words corresponding to the indexes
+# put them into an array of pos_words, neg_words
+rpos_words = count_and_getMostCommon(pos_revs["processedText"])
+rneg_words = count_and_getMostCommon(neg_revs["processedText"])
+spos_words = count_and_getMostCommon(pos_revs["processedSums"])
+sneg_words = count_and_getMostCommon(neg_revs["processedSums"])
+
+print("Review positive words: "+", ".join(rpos_words))
+print("Review negative words: "+", ".join(rneg_words))
+
+print("Summary positive words: "+", ".join(spos_words))
+print("Summary negative words: "+", ".join(sneg_words))
+
+
+#remove common words
+
+[rpos_words,rneg_words] = get_disjoint_elements(rpos_words,rneg_words)
+[spos_words,sneg_words] = get_disjoint_elements(spos_words,sneg_words)
+
+print("UNIQUE Review positive words: "+", ".join(rpos_words))
+print("UNIQUE Review negative words: "+", ".join(rneg_words))
+
+print("UNIQUE Summary positive words: "+", ".join(spos_words))
+print("UNIQUE Summary negative words: "+", ".join(sneg_words))
+
+
+
 sia = sentiment.SentimentIntensityAnalyzer()
 RavgSentiment = x.groupby('asin')["reviewText"].apply(lambda x: np.mean(sia.polarity_scores(x.to_string())["compound"]))
 RavgSentiment = RavgSentiment.fillna(0).rename("Review_Avg_Sentiment")
